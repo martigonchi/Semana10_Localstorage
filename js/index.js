598956a2c00aa92e4282d3c85d1442e3fe1c8dae
martigonchi <martigonchi@gmail.com>
2024-09-22 11:29:02 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,13 @@
 const inputText = document.getElementById('inputText');
 const buttonText = document.getElementById('buttonText');
 
-
 buttonText.addEventListener('click', () => {
     const value = inputText.value;
     
     if (value) {
-       '
         localStorage.setItem('storedText', value);
         alert('Texto guardado en localStorage!');
-    
-        inputText.value = '';
+        inputText.value = '';  
     } else {
         alert('Por favor ingresa algún texto.');
     }
